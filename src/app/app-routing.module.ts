0c0428ccadaa34cf2f47e06e639a61aba83ef014
amierul basyar <amieru.basyar@pmcare.com.my>
2023-08-31 00:20:06 +0800

    Add sample for basic picker

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+// import { BasicComponent } from './basic/basic.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // { path: 'basic', component: BasicComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BasicComponent } from './basic/basic.component';
+import { NgScrollPickerModule } from 'ng-scroll-picker';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, BasicComponent],
+  imports: [BrowserModule, AppRoutingModule, NgScrollPickerModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
